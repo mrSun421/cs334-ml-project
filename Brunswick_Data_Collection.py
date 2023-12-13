ab9842cb5c48d4fc0fd457d6a4e30ad951172a39
Aadi Waghray <aadiwaghray@gmail.com>
2023-12-13 13:34:57 -0500

    Moved lines

--- Brunswick_Data_Collection.py
+++ Brunswick_Data_Collection.py
@@ -31,37 +31,12 @@ def main():
     filepaths = dataset_dir.glob("**/*Brunswick*.csv")
     hourly_data = pd.concat(map(lambda filepath: pd.read_csv(filepath_or_buffer=filepath), filepaths))
 
-
-    # Determine Bad Data in Hourly Dataset Based on Flag and -9999
-    flag_list = ['SOLARAD_FLAG',
-                 'SOLARAD_FLAG',
-                 'SOLARAD_MAX_FLAG',
-                 'SOLARAD_MIN_FLAG',
-                 'SUR_TEMP_FLAG',
-                 'SUR_TEMP_MAX_FLAG',
-                 'SUR_TEMP_MIN_FLAG']
-
-    flagged_indicies = None
-    for flag in flag_list:
-        flagged = np.where(hourly_data[flag] > 0)[0]
-        flagged_indicies = flagged if flagged_indicies is None else np.concatenate([flagged_indicies, flagged])
-
-    flagged_indicies = np.concatenate([np.where(hourly_data['P_CALC'] < 0)[0], flagged_indicies])
-    flagged_indicies = np.unique(flagged_indicies)
-
     # Drop Flags, Dates, Station Identifiers, and Dummy columns
     hourly_data = hourly_data.drop(columns=['WBANNO',
                                             'UTC_DATE',
                                             'UTC_TIME',
                                             'CRX_VN',
-                                            'SOLARAD_FLAG',
-                                            'SOLARAD_MAX_FLAG',
-                                            'SOLARAD_MIN_FLAG',
-                                            'SUR_TEMP_TYPE',
-                                            'SUR_TEMP_FLAG',
-                                            'SUR_TEMP_MAX_FLAG',
-                                            'SUR_TEMP_MIN_FLAG',
-                                            'RH_HR_AVG_FLAG'])
+                                            'SUR_TEMP_TYPE'])
 
     hourly_data.to_csv("./Dataset_Brunswick_Hourly.csv", index=False)
 
--- preprocessing.py
+++ preprocessing.py
@@ -9,6 +9,23 @@ def main():
     LCD_data = pd.read_csv("./Dataset_Brunswick_LCD.csv", dtype=object)
     Hourly_data = pd.read_csv("./Dataset_Brunswick_Hourly.csv", dtype=object)
 
+    # Determine Bad Data in Hourly Dataset Based on Flag and -9999
+    flag_list = ['SOLARAD_FLAG',
+                 'SOLARAD_FLAG',
+                 'SOLARAD_MAX_FLAG',
+                 'SOLARAD_MIN_FLAG',
+                 'SUR_TEMP_FLAG',
+                 'SUR_TEMP_MAX_FLAG',
+                 'SUR_TEMP_MIN_FLAG']
+
+    flagged_indicies = None
+    for flag in flag_list:
+        flagged = np.where(hourly_data[flag] > 0)[0]
+        flagged_indicies = flagged if flagged_indicies is None else np.concatenate([flagged_indicies, flagged])
+
+    flagged_indicies = np.concatenate([np.where(hourly_data['P_CALC'] < 0)[0], flagged_indicies])
+    flagged_indicies = np.unique(flagged_indicies)
+
     # Date Extraction
     LCD_data["DATE"] = pd.to_datetime(LCD_data["DATE"])
     LCD_data["YEAR"] = LCD_data["DATE"].dt.year
