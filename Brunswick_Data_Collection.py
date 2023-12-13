44a777616bde38834e51ceb749b3c834b001f820
Aadi Waghray <aadiwaghray@gmail.com>
2023-12-13 13:26:34 -0500

    Created array of bad indicies

--- Brunswick_Data_Collection.py
+++ Brunswick_Data_Collection.py
@@ -11,17 +11,17 @@ def main():
     # Drop Station Identifiers and Dummy columns
     LCD_data = LCD_data.drop(LCD_data.iloc[:, np.arange(24, len(LCD_data.columns))], axis=1) 
     LCD_data = LCD_data.drop(columns=['STATION',
-                                  'LATITUDE',
-                                  'LONGITUDE',
-                                  'ELEVATION',
-                                  'NAME',
-                                  'SOURCE',
-                                  'HourlyPrecipitation',
-                                  'HourlyPresentWeatherType',
-                                  'HourlyPressureChange',
-                                  'HourlyPressureTendency',
-                                  'HourlySeaLevelPressure',
-                                  'HourlyWindGustSpeed'])
+                                      'LATITUDE',
+                                      'LONGITUDE',
+                                      'ELEVATION',
+                                      'NAME',
+                                      'SOURCE',
+                                      'HourlyPrecipitation',
+                                      'HourlyPresentWeatherType',
+                                      'HourlyPressureChange',
+                                      'HourlyPressureTendency',
+                                      'HourlySeaLevelPressure',
+                                      'HourlyWindGustSpeed'])
 
 
     LCD_data.to_csv("./Dataset_Brunswick_LCD.csv", index=False)
@@ -31,27 +31,37 @@ def main():
     filepaths = dataset_dir.glob("**/*Brunswick*.csv")
     hourly_data = pd.concat(map(lambda filepath: pd.read_csv(filepath_or_buffer=filepath), filepaths))
 
-    # Use flags to elimiate bad data
-    hourly_data = hourly_data.drop(hourly_data[hourly_data['SOLARAD_FLAG'] > 0].index)
-    hourly_data = hourly_data.drop(hourly_data[hourly_data['SOLARAD_MAX_FLAG'] > 0].index)
-    hourly_data = hourly_data.drop(hourly_data[hourly_data['SOLARAD_MIN_FLAG'] > 0].index)
-    hourly_data = hourly_data.drop(hourly_data[hourly_data['SUR_TEMP_FLAG'] > 0].index)
-    hourly_data = hourly_data.drop(hourly_data[hourly_data['SUR_TEMP_MAX_FLAG'] > 0].index)
-    hourly_data = hourly_data.drop(hourly_data[hourly_data['SUR_TEMP_MIN_FLAG'] > 0].index)
+
+    # Determine Bad Data in Hourly Dataset Based on Flag and -9999
+    flag_list = ['SOLARAD_FLAG',
+                 'SOLARAD_FLAG',
+                 'SOLARAD_MAX_FLAG',
+                 'SOLARAD_MIN_FLAG',
+                 'SUR_TEMP_FLAG',
+                 'SUR_TEMP_MAX_FLAG',
+                 'SUR_TEMP_MIN_FLAG']
+
+    flagged_indicies = None
+    for flag in flag_list:
+        flagged = np.where(hourly_data[flag] > 0)[0]
+        flagged_indicies = flagged if flagged_indicies is None else np.concatenate([flagged_indicies, flagged])
+
+    flagged_indicies = np.concatenate([np.where(hourly_data['P_CALC'] < 0)[0], flagged_indicies])
+    flagged_indicies = np.unique(flagged_indicies)
 
     # Drop Flags, Dates, Station Identifiers, and Dummy columns
     hourly_data = hourly_data.drop(columns=['WBANNO',
-                                     'UTC_DATE',
-                                     'UTC_TIME',
-                                     'CRX_VN',
-                                     'SOLARAD_FLAG',
-                                     'SOLARAD_MAX_FLAG',
-                                     'SOLARAD_MIN_FLAG',
-                                     'SUR_TEMP_TYPE',
-                                     'SUR_TEMP_FLAG',
-                                     'SUR_TEMP_MAX_FLAG',
-                                     'SUR_TEMP_MIN_FLAG',
-                                     'RH_HR_AVG_FLAG'])
+                                            'UTC_DATE',
+                                            'UTC_TIME',
+                                            'CRX_VN',
+                                            'SOLARAD_FLAG',
+                                            'SOLARAD_MAX_FLAG',
+                                            'SOLARAD_MIN_FLAG',
+                                            'SUR_TEMP_TYPE',
+                                            'SUR_TEMP_FLAG',
+                                            'SUR_TEMP_MAX_FLAG',
+                                            'SUR_TEMP_MIN_FLAG',
+                                            'RH_HR_AVG_FLAG'])
 
     hourly_data.to_csv("./Dataset_Brunswick_Hourly.csv", index=False)
 
